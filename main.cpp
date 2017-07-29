83d01b956143463d61883fe03da358dab63456e0
Papashanskiy <Apashanskiy@gmail.com>
2017-07-29 16:51:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,14 +10,30 @@
 
 using namespace std;
 
-int main(){
+int main() {
 	
-	Fractions firstFraction;
+	cout << "Fraction numbers v 1.0\nDeveloped by Apashanskiy\n\n";
 
-	firstFraction.setFraction(12, 5);
+	cout << "************************************************************************\n"
+		 << "* This programm can fold only positive fractional numbers.             *\n" <<
+			"* If the fractional  number is negative the programm will stop working!*\n" <<
+			"************************************************************************\n";
 
-	cout << "Our fraction is " << firstFraction.getFraction() << "!\n";
+	
+	
+	Fractions f1, f2, f3, f4;
 
+	f1.setFraction(642, 23);
+	f2.setFraction(172, 63);
+	f3.setFraction(0, 0);
+	
+	
+	f3 = f4.sum(f1, f2, f3);
+
+	cout << "Sum of " << f1.getFraction() << " and " 
+		<< f2.getFraction() << " is " << f3.getFraction() << endl;
+	
+	
 	_getch();
 	return 0;
 }
